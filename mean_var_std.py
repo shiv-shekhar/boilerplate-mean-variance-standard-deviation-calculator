4b4389d06a8bee37c676eccae7cada6fe0aeea3e
Shivam Shekhar <45821075+shiv-shekhar@users.noreply.github.com>
2021-01-09 13:02:50 +0530

    Update mean_var_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,12 +1,14 @@
-import numpy as np
+timport numpy as np
 
 def calculate(list):
-    calculations = dict()
-    mtrx = np.array(list).reshape(3, 3)
-    calculations["mean"] = [mtrx.mean(0).tolist(), mtrx.mean(1).tolist(), mtrx.mean()]
-    calculations["variance"] = [mtrx.var(0).tolist(), mtrx.var(1).tolist(), mtrx.var()]
-    calculations["standared deviation"] = [mtrx.std(0).tolist(), mtrx.std(1).tolist(), mtrx.std()]
-    calculations["sum"] = [mtrx.sum(0).tolist(), mtrx.sum(1).tolist(), mtrx.sum()]
-    calculations["max"] = [mtrx.max(0).tolist(), mtrx.max(1).tolist(), mtrx.max()]
-    calculations["min"] = [mtrx.min(0).tolist(), mtrx.min(1).tolist(), mtrx.min()]
-    return calculations
+    
+        result = dict()
+        mtrx = np.array(list).reshape(3, 3)
+        result["mean"] = [mtrx.mean(0).tolist(), mtrx.mean(1).tolist(), mtrx.mean()]
+        result["variance"] = [mtrx.var(0).tolist(), mtrx.var(1).tolist(), mtrx.var()]
+        result["standared deviation"] = [mtrx.std(0).tolist(), mtrx.std(1).tolist(), mtrx.std()]
+        result["sum"] = [mtrx.sum(0).tolist(), mtrx.sum(1).tolist(), mtrx.sum()]
+        result["max"] = [mtrx.max(0).tolist(), mtrx.max(1).tolist(), mtrx.max()]
+        result["min"] = [mtrx.min(0).tolist(), mtrx.min(1).tolist(), mtrx.min()]
+        
+        return result
